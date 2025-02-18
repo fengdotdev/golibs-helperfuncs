566b43991331ac372c7f2598af8dd3314d4f48f0
fengdotdev <feng@vitruvio.cl>
2025-02-18 01:02:38 -0300

    Create aes256gcmtest.kt

---
+++ sandbox/android/sampleapp/helperfuncs/src/test/kotlin/dev.feng.helperfuncs/secret/aes256gcmtest.kt
@@ -0,0 +1,9 @@
+package dev.feng.helperfuncs.secret
+
+class aes256gcmtest {
+
+    val fooKey64 = "TH+35QFhuho+Cb4AR8IXtvbbLRtf+vsi4nSwFFEyxgY="
+    val j
+    val fooIv64 ="Kfsl1u9tlqLiHudv"
+    val plaintextString = "Hello from Bun with AES-256-GCM!"
+}
